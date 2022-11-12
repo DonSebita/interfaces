8880304d57179f1f55aa0189660f9fc48f2131df
Nicolas-2002 <nrojas2020@alu.uct.cl>
2022-11-11 23:25:54 -0300

    Base

--- Server.py
+++ Server.py
@@ -6,5 +6,24 @@ app = Flask(__name__)
 def index():
     return render_template("app.html")
 
+#1
+@app.route("/Monitoreo")
+def Monitoreo():
+    return render_template("1.html")
+
+#2
+@app.route("/Envio")
+def Envio():
+    return render_template("2.html")
+#3
+@app.route("/marcadores")
+def marcadores():
+    return render_template("3.html")
+#4
+@app.route("/autos")
+def autos():
+    return render_template("4.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0' ,port=8000)
