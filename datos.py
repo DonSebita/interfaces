a5945cc89b94e7c45ff4706d1fbefcd3fb3f5c5e
Sebastian <luxioxbox49@gmail.com>
2022-11-11 21:37:43 -0300

    "pagina principal"

---
+++ datos.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import random as ra,requests as req, time as ti
+
+sURL = 'http://127.0.0.1:800/datos'
+
+def Generate():
+    dData = {
+        '01': ra.randit(+5,+20),
+        '25': ra.randit(+5,+20),
+        '10': ra.randit(+5,+20),
+        'te': ra.randit(-10,+10)
+    }
+    return dData
+while 1:
+    dData = Generate()
+    MyCnx = req.post(sURL,json=dData)
+    ti.sleep(5)
+MyCnx.close()
