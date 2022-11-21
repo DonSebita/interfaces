8fc0cd4289cb2ef50520a50dbd2ef58677687039
Nicolas-2002 <74681749+Nicolas-2002@users.noreply.github.com>
2022-11-21 00:34:46 -0300

    Final

--- static/1.js
+++ static/1.js
@@ -1,14 +1,22 @@
+let pr=[]
+let pr1=[]
+let pr2=[]
+let tm=[]
 function main() {
     $.getJSON("http://127.0.0.1:8000/gra", function(datos) {
-        console.log(datos)
         const grafica = document.querySelector("#material");
         const temperatura = document.querySelector("#temperatura");
-            // Las etiquetas son las que van en el eje X.
-            const etiquetas = ["1", "2"]
+        pr.push(datos["01"])
+        pr1.push(datos["25"])
+        pr2.push(datos["10"])
+        tm.push(datos["te"])
+        function range(size, startAt = 1) {
+    		return [...Array(size).keys()].map(i => i + startAt);
+		};
             // Podemos tener varios conjuntos de datos. Comencemos con uno
             const datos01 = {
                 label: "Material Particulado 0.1",
-                data: [datos["01"],datos["01"]], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: pr, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 borderColor: 'blue',
                 borderWidth: 1,// Ancho del borde
                 tension: 0.1,
@@ -16,7 +24,7 @@ function main() {
             };
             const datos25 = {
                 label: "Material Particulado 2.5",
-                data: [datos["25"],datos["25"]], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: pr1, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 borderColor: 'red',
                 borderWidth: 1,// Ancho del borde
                 tension: 0.1,
@@ -24,7 +32,7 @@ function main() {
             };
             const datos10 = {
                 label: "Material Particulado 10",
-                data: [datos["10"],datos["10"]], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: pr2, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 borderColor: 'green',
                 borderWidth: 1,// Ancho del borde
                 tension: 0.1,
@@ -32,7 +40,7 @@ function main() {
             };
             const datoste = {
                 label: "Temperatura",
-                data: [datos["te"],datos["te"]], // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
+                data: tm, // La data es un arreglo que debe tener la misma cantidad de valores que la cantidad de etiquetas
                 borderColor: 'red',
                 borderWidth: 1,// Ancho del borde
                 tension: 0.1,
@@ -42,33 +50,75 @@ function main() {
             var myChart=new Chart(grafica, {
                 type: 'line',// Tipo de gráfica
                 data: {
-                    labels: etiquetas,
+                    labels: range(pr.length),
                     datasets: [
                         datos01,
                         datos25,
                         datos10,
                         // Aquí más datos...
                     ]
-                },
+                }, options: {
+                    responsive: true,
+                        scales: {
+                            x: {
+                                display:true,
+                                title:{
+                                    display:true,
+                                    text:"N° de muestra",
+                                    color:"#0000ff",
+                                },
+                                    },
+                            y: {
+                                display:true,
+                                title:{
+                                    display:true,
+                                    text:"μg/m^3",
+                                    color:"#0000ff",
+                                },
+                            },	
+                           },
+                           plugins: {
+                                  title: {
+                                    display: true,
+                                    text: 'Niveles de contaminacion',
+                                    fontSize: 50,
+                                    padding:30,
+    
+                                      }
+                                },
+                    },
             });
             var myChart2=new Chart(temperatura, {
                 type: 'line',// Tipo de gráfica
                 data: {
-                    labels: etiquetas,
+                    labels: range(tm.length),
                     datasets: [
                         datoste,
                         // Aquí más datos...
                     ]
                 },
                 options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }],
+                    responsive: true,
+                        scales: {
+                            x: {
+                                display:true,
+                                title:{
+                                    display:true,
+                                    text:"N° de muestra",
+                                    color:"#0000ff",
+                                },
+                                    },
+                           },
+                           plugins: {
+                                  title: {
+                                    display: true,
+                                    text: 'Nivel de Temperatura',
+                                    fontSize: 50,
+                                    padding:30,
+    
+                                      }
+                                },
                     },
-                }
             });
         document.getElementById("exM").addEventListener('click',()=>{
             exportar(myChart)
